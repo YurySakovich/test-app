6f455aab7170e743364b93d2bcbc690fe973ebd0
Yura Sakovich <yury_sakovich@smartexlab.com>
2018-10-13 21:55:04 +0300

    remove parser

--- titan-backend/app.js
+++ titan-backend/app.js
@@ -1,7 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+// var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var mongoose = require('mongoose')
@@ -20,7 +20,7 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, './../titan-front/dist')));
 app.use(cors())
 
